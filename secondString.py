d64268c00375a40084edbf29a2c05d45c2c89895
kalamkitty <g00411363@gmit.ie>
2022-03-22 20:29:59 +0000

    Added references and added spacing

--- secondString.py
+++ secondString.py
@@ -1,6 +1,13 @@
 # A program that asks a user to input a string and outputs every second letter in reverse order
 # Author: Ka Lam (Kitty) Kwan
 
-string = input ("Please enter a sentence:")[::-1]
-secondString = string[::2]
+# REF 1: Printing every other letter of a word
+# https://stackoverflow.com/questions/48873854/python-printing-ever-other-letter-of-a-word
+
+# REF 2 : Reverse a string/ slicing
+# https://www.educative.io/edpresso/how-do-you-reverse-a-string-in-python
+
+
+string = input ("Please enter a sentence: ")[::-1]   # REF 2
+secondString = string[::2]                          # REF 1
 print (secondString)
