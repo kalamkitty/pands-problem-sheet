59a4a815331810846ab0e43919d7fa622585d9ea
Kitty <98161069+kalamkitty@users.noreply.github.com>
2022-03-22 21:02:01 +0000

    Delete weeklyTask05.py

--- weeklyTask05.py
+++
@@ -1,9 +0,0 @@
-# program that outputs wheather or not today is a weekday
-
-import datetime
- 
-weekno = datetime.datetime.today().weekday()
-if weekno < 5:
-    print("Yes, unfortuately today is a Weekday")
-else:  # 5 Sat, 6 Sun
-    print("It is the Weekend,yay!")
