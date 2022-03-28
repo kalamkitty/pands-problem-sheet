db002470e4ced611d96c8ccba1ae7f77d4d6222b
kalamkitty <g00411363@gmit.ie>
2022-03-28 21:50:00 +0100

    added comments

--- weekday.py
+++ weekday.py
@@ -4,10 +4,10 @@
 # REF 1 : Find current day is weekday/weekend
 # https://www.pythonprogramming.in/how-to-find-current-day-is-weekday-or-weekends-in-python.html
 
-import datetime
+import datetime                                            # import daytime module
  
-weekno = datetime.datetime.today().weekday()               # REF 1
-if weekno < 5:
+weekno = datetime.datetime.today().weekday()               # import today as a variable
+if weekno < 5:                                             # if day is less than 5 (weekday) , print
     print("Yes, unfortuately today is a weekday")
-else:                                                      # 5 Sat, 6 Sun
+else:                                                      # 5 Sat, 6 Sun, or else, print
     print("It is the weekend,yay!")
